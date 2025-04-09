7fa89442ce1b89eaa2d0050cdf5f96c2af5239a7
xupypg88 <xupypg88@gmail.com>
2025-04-09 13:05:04 +0300

    Update RoleDefinitionsByObjectId.ps1

--- RoleDefinitionsByObjectId.ps1
+++ RoleDefinitionsByObjectId.ps1
@@ -1,18 +1,23 @@
-$AppObjectId = Read-Host -Prompt "Enter the ObjectId: "
+Connect-AzureAD 
+# app data by object id 
+$principal = Get-AzureADServicePrincipal -Filter "ObjectId eq '671ea22b-034d-412a-bb96-0b279279eb4c'"
+
+$AppObjectId = $principal.ObjectId
 
 #output file path
 $filename = "output.txt"
+$principal | ConvertTo-Json | Out-File -width 300 $filename
 
+#getting roles for the app
 $roles = Get-AzRoleAssignment -ObjectId $AppObjectId
-Write-Host "Collecting role definitions:"
 
+#getting each role data
 ForEach ($role in $roles) {
-	$role | ConvertTo-Json | Out-File -width 300 $filename
+	$role | ConvertTo-Json | Out-File -append -width 300 $filename
 	$defs = Get-AzRoleDefinition -Id $role.RoleDefinitionId 
 	ForEach ($def in $defs) {
 		$def.Name
-		$def | ConvertTo-Json | Out-File -width 300 $filename
+		$def | ConvertTo-Json | Out-File -append -width 300 $filename
 	}
 }
-
 Write-Host "Role Definitions are collected to the file: $filename"
